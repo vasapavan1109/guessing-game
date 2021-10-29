8758fffac80eb936546c48d4b3b88576ca1d96fd
vasapavan1109 <76104116+vasapavan1109@users.noreply.github.com>
2021-10-29 16:49:22 +0530

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,9 @@
+guess = int(input("Guess a number "))
+if(guess>10):
+    print("ur guess was too less")
+if(guess<10):
+    print("Your guess was to low")
+
+if(guess==7<10):
+    print('YOU WIN!')    
+
